0c5ff57ac9b21c546862700303bc46b9d323db00
JhowAndrade <johanny_andrade@yahoo.com.br>
2023-06-16 19:28:11 -0300

    Admin page layout

---
+++ src/routes/ClientHome/Admin/index.tsx
@@ -0,0 +1,13 @@
+import HeaderAdmin from "../../../components/HeaderAdmin";
+import { Outlet } from 'react-router-dom';
+
+export default function Admin() {
+
+return(
+    <>
+        <HeaderAdmin />
+        <Outlet />
+    </>
+)
+
+}
