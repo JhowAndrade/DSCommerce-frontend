679c7b0ec6f2ad652e00b7ac225f4147e602b206
JhowAndrade <johanny_andrade@yahoo.com.br>
2023-06-16 20:56:48 -0300

    isAuthenticated

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,9 +1,10 @@
 import QueryString from "qs";
-import { CredentialsDTO } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECTRET } from "../utils/system";
 import { AxiosRequestConfig } from "axios";
+import { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
 import { requestBackend } from "../utils/request";
 import * as accessTokenRepository from '../localstorage/access-token-repository';
+import jwtDecode from "jwt-decode";
 
 export function loginRequest(loginData: CredentialsDTO) {
 
@@ -13,9 +14,9 @@ export function loginRequest(loginData: CredentialsDTO) {
         Authorization: "Basic" + window.btoa(CLIENT_ID + ":" + CLIENT_SECTRET)
     }
 
-    const requestBody = QueryString.stringify({...loginData, grand_type: "password" });
+    const requestBody = QueryString.stringify({ ...loginData, grand_type: "password" });
 
-    const config : AxiosRequestConfig = {
+    const config: AxiosRequestConfig = {
         method: "POST",
         url: "/oauth/token",
         data: requestBody,
@@ -26,14 +27,30 @@ export function loginRequest(loginData: CredentialsDTO) {
 }
 
 export function logout() {
-accessTokenRepository.remove();
+    accessTokenRepository.remove();
 }
 
 export function saveAccessToken(token: string) {
-accessTokenRepository.save(token);
+    accessTokenRepository.save(token);
 }
 
 export function getAccessToken() {
-   return accessTokenRepository.get();
+    return accessTokenRepository.get();
 }
 
+export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
+    try {
+        const token = accessTokenRepository.get();
+        return token == null ? undefined : (jwtDecode(token) as AccessTokenPayloadDTO);
+    } catch (error) {
+        return undefined;
+    }
+}
+
+export function isAuthenticated(): boolean {
+    let tokenPayload = getAccessTokenPayload();
+    if (tokenPayload && tokenPayload.exp * 1000 > Date.now()) {
+        return true;
+    }
+    return false;
+}
