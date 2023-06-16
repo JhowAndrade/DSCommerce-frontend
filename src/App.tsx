381a3a6315bba08848df1ca124abe913bb06f1d4
JhowAndrade <johanny_andrade@yahoo.com.br>
2023-06-16 20:29:17 -0300

    Redirecting outside React component

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,34 @@ import ProductDetails from "./routes/ClientHome/ProductDetails";
 import Cart from "./routes/ClientHome/Cart";
 import { useState } from "react";
 import { ContextCartCount } from './utils/context-cart';
-import  Login from './routes/ClientHome/Login';
+import Login from './routes/ClientHome/Login';
 import Admin from "./routes/ClientHome/Admin";
 import AdminHome from "./routes/ClientHome/Admin/AdminHome";
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+import { history } from './utils/history';
 
 export default function App() {
 
   const [contextCartCount, setContextCartCount] = useState<number>(0);
 
   return (
-    <ContextCartCount.Provider value={{contextCartCount, setContextCartCount}}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ClientHome />}>
-          <Route index element={<Catalog />} />
-          <Route path="catalog" element={<Catalog />} />
-          <Route path="product-details/:productId" element={<ProductDetails />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="login" element={<Login />} />
-        </Route>
+    <ContextCartCount.Provider value={{ contextCartCount, setContextCartCount }}>
+      <HistoryRouter history={history}>
+        <Routes>
+         <Route path="/" element={<ClientHome />}>
+            <Route index element={<Catalog />} />
+            <Route path="catalog" element={<Catalog />} />
+            <Route path="product-details/:productId" element={<ProductDetails />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="login" element={<Login />} />
+          </Route>
         <Route path="/admin/" element={<Admin />} >
-            <Route index element={<AdminHome />} />
+          <Route index element={<AdminHome />} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </BrowserRouter>
-    </ContextCartCount.Provider>
+    </HistoryRouter>
+    </ContextCartCount.Provider >
   );
 }
 
