9d186dc3ef45ff823233e88f29d591e941470e52
JhowAndrade <johanny_andrade@yahoo.com.br>
2023-06-19 21:56:12 -0300

    Confirmation

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -66,8 +66,6 @@ export default function Cart() {
                                         </div>
                                     ))
                                 }
-
-
                                 <div className="dsc-cart-total-container">
                                     <h3>R$ {cart.total.toFixed(2)}</h3>
                                 </div>
@@ -75,8 +73,6 @@ export default function Cart() {
 
                         )
                 }
-
-
                 <div className="dsc-btn-page-container">
                     <div className="dsc-btn dsc-btn-blue">
                         Finalizar pedido
