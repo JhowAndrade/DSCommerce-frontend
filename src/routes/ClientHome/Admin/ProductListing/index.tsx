41005fa5f0622a1c73025b6d94f5d051309f4d09
JhowAndrade <johanny_andrade@yahoo.com.br>
2023-06-27 21:55:37 -0300

    forms updateAll

--- src/routes/ClientHome/Admin/ProductListing/index.tsx
+++ src/routes/ClientHome/Admin/ProductListing/index.tsx
@@ -8,6 +8,8 @@ import SearchBar from '../../../../components/SearchBar';
 import ButtonNextPage from '../../../../components/ButtonNextPage';
 import DialogInfo from '../../../../components/DialogInfo';
 import DialogConfirmation from '../../../../components/DialogConfirmation';
+import ButtonInverse from '../../../../components/ButtonInverse';
+import { useNavigate } from 'react-router-dom';
 
 
 type QueryParams = {
@@ -16,6 +18,8 @@ type QueryParams = {
 }
 
 export default function ProductListing() {
+
+    const navigate = useNavigate();
 
     const [dialogInfoData, setDialogInfoData] = useState({
         visible: false,
@@ -46,6 +50,10 @@ export default function ProductListing() {
             })
     }, [queryParams]);
 
+    function handleNewProductClick() {
+        navigate("/admin/products/create");
+    }
+
     function handleSearch(seartchText: string) {
         setProducts([]);
         setQueryParams({ ...queryParams, page: 0, name: seartchText });
@@ -56,7 +64,7 @@ export default function ProductListing() {
     }
 
     function handleDialogInfoClose() {
-        setDialogInfoData({ ...dialogConfirmationData, visible: false });
+        setDialogInfoData({ ...dialogInfoData, visible: false });
     }
 
     function handleDeleteClick(productId: number) {
@@ -90,7 +98,10 @@ export default function ProductListing() {
                 <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
 
                 <div className="dsc-btn-page-container dsc-mb20">
-                    <div className="dsc-btn dsc-btn-white">Novo</div>
+                    <div onClick={handleNewProductClick}>
+                        <ButtonInverse text="Novo" />
+                    </div>
+
                 </div>
 
                 <SearchBar onSearch={handleSearch} />
