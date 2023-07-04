255f55acb17cd9e87862641e8c71dc152ce4d8e3
JhowAndrade <johanny_andrade@yahoo.com.br>
2023-07-04 00:02:53 -0300

    FormTextArea

--- src/routes/ClientHome/Admin/ProductForm/index.tsx
+++ src/routes/ClientHome/Admin/ProductForm/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import FormInput from '../../../../components/FormInput';
 import * as forms from '../../../../utils/forms';
 import * as productService from '../../../../services/product-service';
+import FormTextArea from '../../../../components/FormTextArea';
 
 export default function ProductForm() {
 
@@ -41,6 +42,17 @@ export default function ProductForm() {
             name: "imgUrl",
             type: "text",
             placeholder: "Imagem",
+        },
+        description: {
+            value: "",
+            id: "description",
+            name: "description",
+            type: "text",
+            placeholder: "Descrição",
+            validaion: function (value: string) {
+                return /^.{10,}$/.test(value);
+            },
+            message: "A descrição deve ter pelo menos 10 caracteres"
         }
     });
 
@@ -90,15 +102,27 @@ export default function ProductForm() {
                                 <div className="dsc-form-error">{formData.price.message}</div>
                             </div>
                             <div>
-                                <FormInput {...formData.imgUrl} className="dsc-form-control" onTurnDirty={handleTurnDirty} onChange={handleInputChange} />
+                                <FormInput
+                                    {...formData.imgUrl}
+                                    className="dsc-form-control"
+                                    onTurnDirty={handleTurnDirty}
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                            <div>
+                                <FormTextArea
+                                    {...formData.description}
+                                    className="dsc-form-control dsc-textarea"
+                                    onTurnDirty={handleTurnDirty}
+                                    onChange={handleInputChange}
+                                />
+                                <div className="dsc-form-error">{formData.description.message}</div>
                             </div>
                         </div>
-
                         <div className="dsc-product-form-buttons">
                             <Link to="/admin/products">
                                 <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
                             </Link>
-
                             <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
                         </div>
                     </form>
