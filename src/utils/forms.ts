4aba07ea768ff842e095eb45aa6358337ccbde95
JhowAndrade <johanny_andrade@yahoo.com.br>
2023-07-04 23:28:27 -0300

    Dirty and validate all

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -27,7 +27,7 @@ export function validate(inputs: any, name: string) {
 }
 
 export function toDirty(inputs: any, name: string) {
-    return { ...inputs, [name]: { ...inputs[name], dirty: "true"} };
+    return { ...inputs, [name]: { ...inputs[name], dirty: "true" } };
 }
 
 export function updateAndValidate(inputs: any, name: string, newValue: any) {
@@ -38,4 +38,43 @@ export function updateAndValidate(inputs: any, name: string, newValue: any) {
 export function dirtyAndValidate(inputs: any, name: string) {
     const dataDirty = toDirty(inputs, name);
     return validate(dataDirty, name);
+}
+
+export function toDirtyAll(inputs: any) {
+    const newInputs: any = {};
+    for (var name in inputs) {
+        newInputs[name] = { ...inputs[name], dirty: "true" };
+    }
+    return newInputs;
+}
+
+export function validateAll(inputs: any) {
+
+    const newInputs: any = {};
+
+    for (var name in inputs) {
+        if (inputs[name].validation) {
+
+            const isInValid = !inputs[name].validation(inputs[name].value);
+            
+            newInputs[name] = { ...inputs[name], invalid: isInValid.toString() };
+        }
+        else {
+            newInputs[name] = { ...inputs[name] }
+        }
+    }
+    return newInputs;
+}
+
+export function dirtyAndValidateAll(inputs: any) {
+    return validateAll(toDirtyAll(inputs));
+}
+
+export function hasAnyInvalid(inputs: any) {
+    for (var name in inputs) {
+        if (inputs[name].dirty === "true" && inputs[name].invalid === "true") {
+            return true;
+        }
+    }
+   return false;
 }
